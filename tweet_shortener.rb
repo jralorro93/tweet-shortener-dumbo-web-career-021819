447ef86adc669379f3dd52610d885d4f74c45478
Juan Alorro <jralorro93@gmail.com>
2019-02-13 17:25:25 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,52 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+  change = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end 
+
+
+def word_substituter(tweet)
+  tweet.split(" ").map do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end 
+
+def bulk_tweet_shortener(tweets)
+  tweets.map do |words|   
+    puts word_substituter(words)
+  end 
+end 
+
+def selective_tweet_shortener(tweet)
+    if tweet.length > 140
+      word_substituter(tweet)
+    else
+      tweet
+    end
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet.split(" ").map do |phrase|
+    binding.pry
+    if phrase.length > 140
+      word_substituter(phrase[0..140]) + "..."
+    else 
+      phrase
+    end 
+  end.join(" ")
+end
